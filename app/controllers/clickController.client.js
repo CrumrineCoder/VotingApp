c1f92788dbdd6132cd9f1cb3a727f9625af0a2b7
CrumrineCoder <crumrinecoding@gmail.com>
2017-08-31 15:58:13 -0400

    a

--- app/controllers/clickController.client.js
+++ app/controllers/clickController.client.js
@@ -15,4 +15,15 @@
 		}
 		document.addEvenListener('DOMContentLoaded', fn, false); 
 	}
+	function ajaxRequest(method, url, callback){
+		var xmlhttp = new XMLHttpRequest();
+		
+		xmlhttp.onreadystatechange = function(){
+			if(xmlhttp.readyState ===4 && xmlhttp.status ===200){
+				callback(xmlhttp.response);
+			}
+		};
+		xmlhttp.open(method, url, true); 
+		xmlhttp.send(); 
+	}
 })();
