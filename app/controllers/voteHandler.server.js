83073abb4e6e3ab232c59f65ba48c83e0e7c766c
System <none>
2017-09-05 20:14:52 +0000

    Got the backend talking to the front end and vice versa, babysteps

--- app/controllers/voteHandler.server.js
+++ app/controllers/voteHandler.server.js
@@ -1,36 +1,45 @@
 'use strict';
 
+
 function voteHandler (db) {
+  console.log("Vote Handler running");
+  // Get the 'votes' collection
    var votes = db.collection('votes');
+  // Make a new method 'getvotes'
    this.getvotes = function (req, res) {
+     console.log("Get Votes running");
+     // No need for an id yet, we will need those for when we're allowed to make polls however
       var voteProjection = { '_id': false };
+     // Inside the collection, find the first thing. Will need to change this when there is more than one poll
       votes.findOne({}, voteProjection, function (err, result) {
          if (err) {
             throw err;
          }
-
+        // If there is a result, return it
          if (result) {
             res.json(result);
          } else {
-            votes.insert({ 'votes': 0 }, function (err) {
+           // If there is no result, make a new poll. Will need to change this functionality later. 
+            votes.insert({ 'Yes': 0, 'No': 0 }, function (err) {
                if (err) {
                   throw err;
                }
-
+              // Now that it's made, NOW find it
                votes.findOne({}, voteProjection, function (err, doc) {
                   if (err) {
                      throw err;
                   }
-
                   res.json(doc);
                });
             });
          }
       });
    };
-
+// Vote on the poll 
    this.addvote = function (req, res) {
-      votes.findAndModify({}, { '_id': 1 }, { $inc: { 'votes': 1 }}, function (err, result) {
+     console.log("DEY DEY DEY");
+     // changes 'votes' to the options inside the Poll
+      votes.findAndModify({}, { '_id': 1 }, { $inc: { 'Yes': 1 }}, function (err, result) {
          if (err) {
             throw err;
          }
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,15 +1,16 @@
-//'use strict';
+'use strict';
 
 var VoteHandler = require(process.cwd() + '/app/controllers/voteHandler.server.js');
 
 module.exports = function (app, db) {
+// Creates an object from the server controller
    var voteHandler = new VoteHandler(db);
-
+// If routed here and there's a get method, send the index.html file
    app.route('/')
       .get(function (req, res) {
          res.sendFile(process.cwd() + '/public/index.html');
       });
-
+// If routed here and there's a get or post method, do these  things  inside of the server file
    app.route('/api/votes')
       .get(voteHandler.getvotes)
       .post(voteHandler.addvote)
