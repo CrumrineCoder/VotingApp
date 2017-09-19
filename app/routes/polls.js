bca48a525c85ecd1c741859cf32f4ce53cba98b6
System <none>
2017-09-19 19:20:41 +0000

    About to change poll routing, backup

--- app/routes/polls.js
+++ app/routes/polls.js
@@ -11,17 +11,20 @@ router.get('/create', function(req, res) {
 });
 // Create poll
 router.post('/create', function(req, res) {
-
+    console.log(req.body);
     var question = req.body.question;
+   // Dynamic needed
     var answer = req.body.answer;
     var answer2 = req.body.answer2;
-
-
+// One Solution is to make answer3 - answer10 since they'll be optional and I can limit i in client.js to 10
+    var answer3 = req.body.answer3;
     // Validation
     req.checkBody('question', 'Question is required').notEmpty();
+   // Dynamic needed
     req.checkBody('answer', 'At least two answers are required').notEmpty();
     req.checkBody('answer2', 'At least two answers are required').notEmpty();
     // I might need more validation, but I believe I'll only require these validations and the rest of things that are added are optional, which means I don't need to check them
+  // I suppose I could have the first two be required,but it'd be better if it actually checked if 2 were filled out in case the user was cheeky
 
     var errors = req.validationErrors();
     if (errors) {
@@ -31,8 +34,10 @@ router.post('/create', function(req, res) {
     } else {
         var newPoll= new Poll({
             question: question,
+           //Dynamic needed
             answer: answer,
-            answer2: answer2
+            answer2: answer2,
+            answer3: answer3
         });
         Poll.createPoll(newPoll, function(err, Poll) {
             if (err) throw err;
