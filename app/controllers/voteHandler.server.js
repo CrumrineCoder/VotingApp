abe00212d90a91e91a774c9ddacc0520bc58b8ea
System <none>
2017-09-23 17:48:36 +0000

    Removed vestigial getVotes function, updated readme, and began work on search bar functionality

--- app/controllers/voteHandler.server.js
+++ app/controllers/voteHandler.server.js
@@ -27,46 +27,7 @@ function voteHandler(db) {
             res.json(result);
         });
 
-    };
-
-  
-    // Get the 'votes' collection
-    var votes = db.collection('votes');
-    // Make a new method 'getvotes'
-    this.getvotes = function(req, res) {
-        // No need for an id yet, we will need those for when we're allowed to make polls however
-        var voteProjection = {
-            '_id': false
-        };
-        // Inside the collection, find the first thing. Will need to change this when there is more than one poll
-        votes.findOne({}, voteProjection, function(err, result) {
-            if (err) {
-                throw err;
-            }
-            // If there is a result, return it
-            if (result) {
-                res.json(result);
-            } else {
-                // If there is no result, make a new poll. Will need to change this functionality later. 
-                votes.insert({
-                    'Yes': 0,
-                    'No': 0
-                }, function(err) {
-                    if (err) {
-                        throw err;
-                    }
-                    // Now that it's made, NOW find it
-                    votes.findOne({}, voteProjection, function(err, doc) {
-                        if (err) {
-                            throw err;
-                        }
-                        res.json(doc);
-                    });
-                });
-            }
-        });
-    };
-   
+    };   
 }
 
 module.exports = voteHandler;
