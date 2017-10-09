13516b5b0e09e187c7d4dd4dc346174638708e70
System <none>
2017-10-09 23:18:31 +0000

    Vote button is disabled until the question is entered and two replies are entered

--- app/controllers/creatingPolls.js
+++ app/controllers/creatingPolls.js
@@ -19,5 +19,24 @@ function handler(e) {
     form.insertBefore(input, submitButton);
     latestInputField = document.getElementsByName("answer" + i)[0];
     latestInputField.addEventListener("input", handler);
-}
+} 
 
+function checkReplies(){
+    var replies = 0;
+       var question = document.getElementById('writeQuestion'); 
+        $('input').each(function() {
+            if ($(this).val() != '' && $(this).attr('name')!='question' && $(this).attr('name')!='searchTerm' && $(this).attr('type') == 'text') {
+                replies++;
+            }
+        });
+        if (replies < 2 || question.value == '') {
+          $('.submitButton').addClass('disabled');
+            $('.submitButton').attr('disabled', 'disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
+        } else {
+              $('.submitButton').removeClass('disabled');
+            $('.submitButton').removeAttr('disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
+        }
+  
+}
+checkReplies();
+ $('input').keyup(checkReplies);
