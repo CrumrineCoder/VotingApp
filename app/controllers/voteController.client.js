a7684794fabb96840338fa56759dde3d774d64b9
Glitch (joinordie) <none>
2018-02-09 22:28:42 +0000

    Removed decimal scaling on Charts

--- app/controllers/voteController.client.js
+++ app/controllers/voteController.client.js
@@ -272,7 +272,13 @@
                  scales: {
                      xAxes: [{
                          ticks: {
-                             beginAtZero: true
+                             beginAtZero: true,
+                           userCallback: function(label, index, labels) {
+                             // when the floored value is the same as the value we have a whole number
+                             if (Math.floor(label) === label) {
+                               return label;
+                             }
+                           }
                          }
                      }]
                  },
